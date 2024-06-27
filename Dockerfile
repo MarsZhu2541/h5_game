c2dc02301516613395e36977010d17180d48fb3a
Mars Zhu <mars.zhu@valtech.com>
2024-06-27 13:53:55 +0800

    use new index

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,8 @@ FROM nginx
 LABEL authors="marszhu"
 COPY ./default.conf /etc/nginx/conf.d/default.conf
 COPY . /usr/share/nginx/html
+
+COPY ./ssl/503666666.cn.pem /etc/nginx/certs/503666666.cn.pem
+COPY ./ssl/503666666.cn.key /etc/nginx/certs/503666666.cn.key
+
 RUN chmod -R 755 /usr/share/nginx/html
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker stop gamecollection
-docker rm gamecollection
-docker build -t marszhu2541/gamecollection .
+#docker stop gamecollection
+#docker rm gamecollection
+#docker build -t marszhu2541/gamecollection .
 docker push marszhu2541/gamecollection
-#docker run --name gamecollection -p 80:80 -d marszhu2541/gamecollection
+#docker run --name gamecollection -p 80:80 -p 443:443 -d marszhu2541/gamecollection
 
