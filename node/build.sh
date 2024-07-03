096ffce90ff9ff98f832fa08b5a0942b8002b299
Mars Zhu <mars.zhu@valtech.com>
2024-07-03 17:05:02 +0800

    adapt s11

--- node/build.sh
+++ node/build.sh
@@ -4,4 +4,4 @@ docker stop mynode
 docker rm mynode
 docker build -t marszhu2541/mynode .
 #docker push marszhu2541/mynode
-docker run --name mynode -p 3000:3000 -d marszhu2541/mynode
+docker run --name mynode -p 8000:3000 -d marszhu2541/mynode
